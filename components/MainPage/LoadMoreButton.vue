477c7c3c875212d6bf26c83969462a88c9802cab
danasomethin <hkdana@hotmail.com>
2025-04-04 17:17:00 +0800

    Fixed load more button

--- components/MainPage/LoadMoreButton.vue
+++ components/MainPage/LoadMoreButton.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="container" @click="props.retrieveNewPokemonList">
-    <h5>Load more</h5>
+  <div class="container">
+    <span class="button" @click="props.retrieveNewPokemonList">
+      <h5>Load more</h5>
+    </span>
   </div>
 </template>
 
@@ -14,18 +16,27 @@ const props = defineProps<Props>();
 
 <style scoped lang="scss">
 .container {
+  @extend .center;
   width: 100%;
+}
+.button {
+  @extend .center;
+
+  width: 100%;
+  height: 56px;
   background-color: var(--button-primary-color);
   border-radius: 8px;
   padding: 12px;
 
-  h5 {
-    text-align: center;
-  }
-
-  :hover {
+  &:hover {
     background-color: var(--button-primary-hover-color);
     cursor: pointer;
   }
+}
+
+.center {
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
